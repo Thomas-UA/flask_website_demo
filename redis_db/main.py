0dc640b58eea22710563eac689e4dd8749fc6572
Thomas-UA <faustpear@gmail.com>
2022-11-01 09:48:38 +0000

    add some changes to run app on gcloud

--- redis_db/main.py
+++ redis_db/main.py
@@ -6,25 +6,22 @@ from redis_db import USERS
 from datetime import datetime
 
 
-r = redis.Redis(charset="utf-8", decode_responses=True)
-# host="localhost", port=6379, db=0
+r = redis.Redis(charset="utf-8", decode_responses=True, host="localhost", port=6379, db=0)
 
-with r.pipeline() as pipe:
-    for user in USERS:
-        pipe.set(
-            name=user.get("id"),
-            value=json.dumps(
-                {
-                    "email": user.get("email"),
-                    "password": user.get("password"),
-                    "name": user.get("name"),
-                    "registration_date": str(datetime.now()),
-                    "admin_role": user.get("admin_role", False),
-                    "favorite": user.get("favorite", "Do not declarated")
-                }
-            ),
-            # ex=360
-        )
-    pipe.execute()
+for user in USERS:
+    r.set(
+        name=user.get("id"),
+        value=json.dumps(
+            {
+                "email": user.get("email"),
+                "password": user.get("password"),
+                "name": user.get("name"),
+                "registration_date": str(datetime.now()),
+                "admin_role": user.get("admin_role", False),
+                "favorite": user.get("favorite", "Do not declarated")
+            }
+        ),
+        # ex=360
+    )
 
 r.bgsave()
